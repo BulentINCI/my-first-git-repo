f2f051cfbfbf90e336788677d31021620e9f5683
Bülent inci <bulent.inci90@gmail.com>
2022-07-19 15:22:54 +0300

    hello class

--- src/first_git_package/LocalClass.java
+++ src/first_git_package/LocalClass.java
@@ -5,6 +5,7 @@ public class LocalClass {
         System.out.println("This is my first print code");
         System.out.println("This is third commit from local master");
         System.out.println("Remote master update");
+        // Hello Class
 
     }
 }
