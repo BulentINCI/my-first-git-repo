6ac3c7d3dd7036831485aa6d94802a32da620f14
Bülent inci <bulent.inci90@gmail.com>
2022-07-19 12:07:25 +0300

    third commit

--- src/first_git_package/MyGitClass.java
+++ src/first_git_package/MyGitClass.java
@@ -5,5 +5,7 @@ public class MyGitClass{
     public static void main(String[] args) {
 
         System.out.println("I am leraning Github!!");
+        System.out.println("I am adding  new  code...");
+        System.out.println("I am addig one more");
     }
 }
