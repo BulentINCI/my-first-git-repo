6b3339a7ffa89634c2d25086fa9869cb24436515
BulentINCI <108153340+BulentINCI@users.noreply.github.com>
2022-07-19 14:46:54 +0300

    last update

--- src/first_git_package/LocalClass.java
+++ src/first_git_package/LocalClass.java
@@ -3,6 +3,11 @@ package first_git_package;
 public class LocalClass {
     public static void main(String[] args) {
         System.out.println("This is my first print code");
+        
+        
+        
+        
+        
         System.out.println("Remote master update");
     }
 }
